5c669d990f7edd601724d3c021dec43533cbbdfe
fasciola <faisalmousa86@gmail.com>
2026-06-13 09:27:19 +0400

    Add extra client list

---
+++ src/extraClients.ts
@@ -0,0 +1,9 @@
+import type { ClientData } from './config';
+
+export const extraClients: ClientData[] = [
+  { name: 'Jinko Solar', nameAr: 'جينكو سولار', logo: 'logos/jinko-solar.png' },
+  { name: 'Bee', nameAr: 'بي', logo: 'logos/bee.png' },
+  { name: 'Maa Althahab', nameAr: 'ماء الذهب', logo: 'logos/maa-althahab.png' },
+  { name: 'Royal Ceramica', nameAr: 'رويال سيراميكا', logo: 'logos/royal-ceramica.png' },
+  { name: 'Africa and Gulf Bank', nameAr: 'بنك أفريقيا والخليج', logo: 'logos/africa-gulf-bank.png' },
+];
